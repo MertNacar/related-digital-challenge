f0f372c5a371256a56f4c3b4eaf4ad4d546772ca
mertnacar <mertnacar2374@gmail.com>
2021-09-09 16:02:21 +0300

    adde draft action components

---
+++ src/components/Action/ActionButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <button @click="onAction">{{ title }}</button>
+</template>
+
+<script>
+export default {
+  name: "ActionButton",
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    onAction: {
+      type: Function,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ src/components/Action/ActionModal.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>ActionModal</div>
+</template>
+
+<script>
+export default {
+  name: "ActionModal",
+};
+</script>
